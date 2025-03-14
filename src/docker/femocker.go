1fbf247c7ffff47496c3584fc2fc92e78af733f1
Twopic2 <bowenzhang0@gmail.com>
2025-03-14 02:21:58 -0700

    Added UWU support

--- src/docker/femocker.go
+++ src/docker/femocker.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 )
 
 func psContainers(cli *client.Client) ([]types.Container, error) {
@@ -47,6 +49,76 @@ func psStats(cli *client.Client, containerID string) (float64, float64, error) {
 
 }
 
+func showGraph(cli *client.Client) {
+	if err := ui.Init(); err != nil {
+		log.Fatal("Failed to initialize termui:", err)
+	}
+	defer ui.Close()
+
+	cpuBar := widgets.NewBarChart()
+	cpuBar.Title = "CpUWU (>w<) %"
+	cpuBar.BarWidth = 6
+	cpuBar.BarColors = []ui.Color{ui.ColorRed}
+	cpuBar.NumFormatter = func(n float64) string {
+		return fmt.Sprintf("%.2f%%", n)
+	}
+
+	memBar := widgets.NewBarChart()
+	memBar.Title = "MemUSSY •⩊• %"
+	memBar.BarWidth = 10
+	cpuBar.BarColors = []ui.Color{ui.ColorBlue}
+	memBar.NumFormatter = func(n float64) string {
+		return fmt.Sprintf("%.2f%%", n)
+	}
+
+	grid := ui.NewGrid()
+
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
+	grid.Set(ui.NewRow(0.5, cpuBar), ui.NewRow(0.5, memBar))
+
+	timer := time.NewTicker(3 * time.Second)
+
+	defer timer.Stop()
+
+	for {
+		select {
+		case <-timer.C:
+			containers, err := psContainers(cli)
+			if err != nil {
+				log.Fatal()
+				continue
+			}
+
+			labels := []string{}
+			cpuData := []float64{}
+			memData := []float64{}
+
+			for _, con := range containers {
+				cpu, mem, err := psStats(cli, con.ID)
+				if err != nil {
+					log.Printf("Error getting stats for container %s: %v\n", con.ID[:12], err)
+					continue
+				}
+				labels = append(labels, con.Names[0])
+				cpuData = append(cpuData, cpu)
+				memData = append(memData, mem)
+			}
+
+			cpuBar.Labels = labels
+			cpuBar.Data = cpuData
+			memBar.Labels = labels
+			memBar.Data = memData
+
+			ui.Render(grid)
+
+		}
+
+	}
+
+}
+
+/**
 func showContainers(cli *client.Client) {
 	for {
 		fmt.Println("\nFetching container stats...")
@@ -68,12 +140,13 @@ func showContainers(cli *client.Client) {
 	}
 
 }
+	**/
 
 func main() {
-	stat, err := client.NewClientWithOpts((client.FromEnv))
+	stat, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		log.Fatal("Error with Main method", err)
 	}
 
-	showContainers(stat)
+	showGraph(stat)
 }
--- src/docker/go.mod
+++ src/docker/go.mod
@@ -2,7 +2,10 @@ module main
 
 go 1.23.6
 
-require github.com/docker/docker v20.10.24+incompatible
+require (
+	github.com/docker/docker v20.10.24+incompatible
+	github.com/gizak/termui/v3 v3.1.0
+)
 
 require (
 	github.com/Microsoft/go-winio v0.4.14 // indirect
@@ -10,11 +13,15 @@ require (
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/moby/term v0.5.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
