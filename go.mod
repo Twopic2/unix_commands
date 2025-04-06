509e477bb18e3e346ee14fcbe93bd0504bda1bc7
twopic2 <bowenzhang0@gmail.com>
2025-04-05 23:49:04 -0700

    Added femsync to brnach

--- go.mod
+++ go.mod
@@ -1,8 +1,26 @@
 module main
 
-go 1.23.6
+go 1.24
+
+toolchain go1.24.2
 
 require golang.org/x/net v0.37.0
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/gokrazy/rsync v0.2.7 // indirect
+	github.com/google/renameio/v2 v2.0.0 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/landlock-lsm/go-landlock v0.0.0-20250303204525-1544bccde3a3 // indirect
+	github.com/mmcloughlin/md4 v0.1.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.27.6 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	kernel.org/pub/linux/libs/security/libcap/psx v1.2.70 // indirect
+)
 
 require (
 	github.com/bramvdbogaerde/go-scp v1.5.0
--- src/rsync/femsync.go
+++ src/rsync/femsync.go
@@ -1,11 +1,85 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
+// This isn't horrible but I'm tired
+func computeMD5(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
 
-	h_md5 := md5.New()
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+func syncFile(ctx context.Context, wg *sync.WaitGroup, serverAddr string, file string, dest string) {
+	defer wg.Done()
+
+	remote := fmt.Sprintf("%s:%s", serverAddr, dest)
+	cmd := exec.CommandContext(ctx, "rsync", "-avz", "-e", "ssh", file, remote)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Printf("[ERROR] rsync failed for %s: %v\nOutput: %s\n", file, err, string(out))
+		return
+	}
+
+	md5sum, err := computeMD5(file)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Printf("[OK] Synced %s → %s with MD5 %s\n", file, remote, md5sum)
+	}
+}
+
+func main() {
+	var (
+		dir        string
+		serverAddr string
+		destPath   string
+	)
+
+	fmt.Println("Welcome to femSync!")
+
+	flag.StringVar(&dir, "src", ".", "Directory to backup")
+	flag.StringVar(&serverAddr, "server", "user@remote", "Remote SSH server (user@host)")
+	flag.StringVar(&destPath, "destination", "/backup", "Destination path on remote")
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	ctx := context.Background()
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+
+		wg.Add(1)
+		go syncFile(ctx, &wg, serverAddr, path, destPath)
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Wait()
 
 }
