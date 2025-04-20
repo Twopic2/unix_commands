a8d6a8a46e443c09e2db44e6f195f4c3df260da1
twopic2 <bowenzhang0@gmail.com>
2025-04-20 01:35:17 -0700

    curl commit

--- src/curl/uwurl.go
+++ src/curl/uwurl.go
@@ -4,15 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+)
+
+var (
+	O   = flag.String("O", "output", "O is the output file")
+	GET = flag.String("GET", "", "GET protocol")
 )
 
 func main() {
 
 	fmt.Println("Welcome to UWURL!")
-
-	GET := flag.String("GET", "", "GET protocol")
 	flag.Parse()
 
 	if *GET == "" {
@@ -20,26 +24,51 @@ func main() {
 		os.Exit(1)
 	}
 
+	err := download(*GET, *O)
+
+	if err != nil {
+		fmt.Print("doesn't work")
+		os.Exit(1)
+
+	}
+
+	fmt.Printf("Download completed! Saved to %s\n", *O)
+
+}
+
+func download(url, filename string) error {
+
+	fmt.Printf("Making GET request to: %s\n", url)
+
 	client := &http.Client{}
 
-	fmt.Printf("Making GET request to: %s\n", *GET)
-
-	resp, err := client.Get(*GET)
-
+	resp, err := client.Get(url)
 	if err != nil {
 		fmt.Print(nil)
 		os.Exit(1)
+
 	}
 
 	defer resp.Body.Close()
 
-	fmt.Printf("Status: %s\n\n", resp.Status)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Errorf("bad status: %s", resp.Status)
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Print(nil)
-		os.Exit(1)
 	}
 
-	fmt.Println(string(body))
+	output, err := os.Create(filename)
+
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	_, err = io.Copy(output, resp.Body)
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	return nil
+
 }
